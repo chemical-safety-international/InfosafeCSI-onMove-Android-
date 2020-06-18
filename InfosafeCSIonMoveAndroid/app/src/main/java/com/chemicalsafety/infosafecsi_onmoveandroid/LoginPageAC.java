f6acde410b49bccf992c60c2f1abd275b407c66e
Releski <releski@hotmail.com>
2020-06-18 11:34:37 +1000

    Added check version function

--- InfosafeCSIonMoveAndroid/app/src/main/java/com/chemicalsafety/infosafecsi_onmoveandroid/LoginPageAC.java
+++ InfosafeCSIonMoveAndroid/app/src/main/java/com/chemicalsafety/infosafecsi_onmoveandroid/LoginPageAC.java
@@ -8,7 +8,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.text.InputType;
 import android.util.Base64;
 import android.util.Log;
 import android.view.MotionEvent;
--- InfosafeCSIonMoveAndroid/app/src/main/java/com/chemicalsafety/infosafecsi_onmoveandroid/StartupPageAC.java
+++ InfosafeCSIonMoveAndroid/app/src/main/java/com/chemicalsafety/infosafecsi_onmoveandroid/StartupPageAC.java
@@ -1,19 +1,60 @@
 package com.chemicalsafety.infosafecsi_onmoveandroid;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Button;
+
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+
 
 public class StartupPageAC extends AppCompatActivity {
 
 //    private Button welcomeBtn;
+    Button welcomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        welcomeButton = findViewById(R.id.welcomeBtn);
+
+//        Log.i("Local version:", getCurrentVersion());
+//        Log.i("BudleID:", BuildConfig.APPLICATION_ID );
+        try {
+            final String googlePlayStoreVersion = new GetLatestVersion().execute().get();
+//            Log.i("Google play version:", googlePlayStoreVersion);
+
+            float gpVersion = Float.parseFloat(googlePlayStoreVersion);
+            float cVersion = Float.parseFloat(getCurrentVersion());
+
+            gpVersion = Float.parseFloat(new DecimalFormat("##.#").format(gpVersion));
+            cVersion = Float.parseFloat(new DecimalFormat("##.#").format(cVersion));
+
+//            Log.i("changed local version:", "" + cVersion);
+//            Log.i("changed store version:", "" + gpVersion);
+
+            if (gpVersion != cVersion && gpVersion > cVersion) {
+                alertUpdateInfo();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
 
@@ -23,5 +64,76 @@ public class StartupPageAC extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //get local app version
+    private String getCurrentVersion() {
+        PackageManager pm = this.getPackageManager();
+        PackageInfo pInfo = null;
 
+        try {
+            pInfo = pm.getPackageInfo(this.getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        String currentVersion = pInfo.versionName;
+
+        return currentVersion;
+    }
+
+    //get Google play store app version
+    private class GetLatestVersion extends AsyncTask<String, String, String> {
+        String storeVersion;;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+                storeVersion = Jsoup.connect("https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID + "&hl=en")
+                        .timeout(30000)
+                        .userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
+                        .referrer("http://www.google.com")
+                        .get()
+                        .select("div.hAyfc:nth-child(4) > span:nth-child(2) > div:nth-child(1) > span:nth-child(1)")
+                        .first()
+                        .ownText();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return storeVersion;
+
+        }
+
+    }
+
+    private void alertUpdateInfo() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Find new version. Click OK to update now.");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                final String appPackageName = getPackageName(); // getPackageName() from Context or Activity object
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                } catch (android.content.ActivityNotFoundException anfe) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+                }
+
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                welcomeButton.setVisibility(View.INVISIBLE);
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.setCanceledOnTouchOutside(false);
+        alert.show();
+    }
 }
